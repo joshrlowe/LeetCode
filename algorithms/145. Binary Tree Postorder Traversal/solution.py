e4573198ab7a7d52f113614ebdf2576259894536
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-21 16:23:54 -0400

    145. Binary Tree Postorder Traversal

---
+++ algorithms/145. Binary Tree Postorder Traversal/solution.py
@@ -0,0 +1,9 @@
+def postorderTraversal(root):
+    def dfs(node):
+        if node:
+            dfs(node.left)
+            dfs(node.right)
+            result.append(node.val)
+    result = []
+    dfs(root)
+    return result
