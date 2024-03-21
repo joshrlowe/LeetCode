f3e42d5bc18f30bd49733419c317af323bead6cb
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-21 15:26:40 -0400

    20. Valid Parentheses

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Note: "🔒" means you need to subscribe to LeetCode Premium to view the problem.
 | Number | Title | Solution | Difficulty |
 |:-------|:------|:---------|:-----------|
+| 20 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/description/) | [Python](./algorithms/20.%20Valid%20Parentheses/solution.py) | Easy |
+
---
+++ algorithms/20. Valid Parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closing_parentheses = {")" : "(", "}" : "{", "]" : "["}
+        for char in s:
+            if char in closing_parentheses:
+                if stack and stack[-1] == closing_parentheses[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return True if not stack else False
