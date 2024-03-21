8047b679163fc402847e829bd7afcd7d2a844463
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-21 16:28:13 -0400

    189. Rotate Array

--- README.md
+++ README.md
@@ -21,3 +21,4 @@ Note: "🔒" means you need to subscribe to LeetCode Premium to view the problem
 | 145 | [Binary Tree Postorder Traversal](https://leetcode.com/problems/binary-tree-postorder-traversal/) | [Python](./algorithms/145.%20Binary%20Tree%20Postorder%20Traversal/solution.py) | Easy |
 | 155 | [Min Stack](https://leetcode.com/problems/min-stack/) | [Python](./algorithms/155.%20Min%20Stack/solution.py) | Medium |
 | 169 | [Majority Element](https://leetcode.com/problems/majority-element/) | [Python](./algorithms/169.%20Majority%20Element/solution.py) | Easy |
+| 189 | [Rotate Array](https://leetcode.com/problems/rotate-array/) | [Python](./algorithms/189.%20Rotate%20Array/solution.py) | Medium |
---
+++ algorithms/189. Rotate Array/solution.py
@@ -0,0 +1,30 @@
+from typing import List
+
+#
+class Solution1:
+    # Self-created approach - good if I wanted to show I know how reverse could be implemented for indices
+    # O(n) time complexity
+    # O(1) space complexity
+    def rotate(self, nums: List[int], k: int) -> None:
+        def reverse_indices(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        k %= len(nums)
+        nums.reverse()
+        reverse_indices(0, k - 1)
+        reverse_indices(k, len(nums) - 1)
+
+
+class Solution2:
+    # More Pythonic Approach
+    # O(n) Time Complexity
+    # O(1) Space Complexity - List slicing does not use extra space because of Python's efficient implementation, which avoids memory allocation
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums)
+        if k > 0:
+            nums.reverse()
+            nums[:k] = nums[:k][::-1]
+            nums[k:] = nums[k:][::-1]
