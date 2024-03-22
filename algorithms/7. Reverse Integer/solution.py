f5c9acd3941c27b10ed579cdc3418aeca9ff27b5
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-21 23:37:37 -0400

    7. Reverse Integer

---
+++ algorithms/7. Reverse Integer/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = False
+        if x < 0:
+            negative = True
+            x *= -1
+        reversed = 0
+        while x > 0:
+            reversed += x % 10
+            x -= x % 10
+            x /= 10
+            reversed *= 10
+        x = -1 * int(reversed // 10) if negative else int(reversed // 10) 
+        return x if x.bit_length() < 32 else 0
