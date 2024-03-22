075ae10514fa5db4a011df50f0d6d465bb204751
Josh Lowe <joshlowe.cs@gmail.com>
2024-03-21 22:33:27 -0400

    98. Validate Binary Search Tree

---
+++ algorithms/98. Validate Binary Search Tree/solution.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def isSorted(lst):
+            for i in range(len(lst) - 1):
+                if lst[i] >= lst[i + 1]:
+                    return False
+            return True
+
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                result.append(node.val)
+                inorder(node.right)
+
+        result = []
+        inorder(root)
+        return isSorted(result)
