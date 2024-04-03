1dc5baacf76a9cf98122b8a28f5f2f3192b7ab5f
Josh Lowe <joshlowe.cs@gmail.com>
2024-04-03 12:25:51 -0400

    912. Sort an Array

---
+++ algorithms/912. Sort an Array/solution.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        frequency = {}
+        for num in nums:
+            if num not in frequency:
+                frequency[num] = 1
+            else:
+                frequency[num] += 1
+
+        min_element = min(nums)
+        max_element = max(nums)
+
+        index = 0
+        for i in range(min_element, max_element + 1):
+            if i in frequency:
+                while frequency[i] != 0:
+                    nums[index] = i
+                    frequency[i] -= 1
+                    index += 1
+
+        return nums
+        
