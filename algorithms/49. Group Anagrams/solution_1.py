3341e2e9279c4008cb74a8cb18b9a96924b205ef
Josh Lowe <joshlowe.cs@gmail.com>
2024-04-08 20:27:51 +0000

    49. Group Anagrams

---
+++ algorithms/49. Group Anagrams/solution_1.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for s in strs:
+            result[''.join(sorted(s))].append(s)
+        return result.values()
