b233b6b3df3825b150e8b3a3c67aac46c388f6ba
Josh Lowe <joshlowe.cs@gmail.com>
2024-06-11 10:03:18 +0000

    2130. Maximum Twin Sum of a Linked List

--- algorithms/2130. Maximum Twin Sum of a Linked List/solution.py
+++ algorithms/2130. Maximum Twin Sum of a Linked List/solution.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
@@ -13,21 +15,24 @@ class Solution:
             stack.append(slow.val)
             slow = slow.next
             fast = fast.next.next
-        
+
         maxSum = 0
         while slow:
             curSum = slow.val + stack.pop()
             maxSum = max(maxSum, curSum)
             slow = slow.next
-    
+
         return maxSum
-    
+
 
 def run_test_case(test_case_num, head, expected):
     solution = Solution()
     result = solution.pairSum(head)
-    assert result == expected, f"Test case {test_case_num} failed: expected {expected}, got {result}"
+    assert (
+        result == expected
+    ), f"Test case {test_case_num} failed: expected {expected}, got {result}"
     print(f"Test case {test_case_num} - Passed")
+
 
 def test_pairSum():
     solution = Solution()
@@ -62,8 +67,6 @@ def test_pairSum():
     head.next.next.next = ListNode(1)
     run_test_case(4, head, 5)
 
+
 if __name__ == "__main__":
     test_pairSum()
-
-
-        
