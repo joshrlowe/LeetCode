faf531e1b442b558b41df7bebe80bacdfd7f2c19
Josh Lowe <joshlowe.cs@gmail.com>
2024-06-18 17:41:30 +0000

    104. Maximum Depth of Binary Tree - BFS and Iterative DFS Solutions

--- algorithms/104. Maximum Depth of Binary Tree/solution_3.py
+++ algorithms/104. Maximum Depth of Binary Tree/solution_3.py
@@ -1,5 +1,9 @@
 from typing import Optional
 
+"""
+Iterative DFS Solution
+"""
+
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -9,19 +13,18 @@ class TreeNode:
 
 
 class Solution:
-    # Time Complexity: O(n)
-    # Space Complexity: O(n), Omega(log(n))
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        elif root.left and not root.right:
-            return 1 + self.maxDepth(root.left)
-        elif not root.left and root.right:
-            return 1 + self.maxDepth(root.right)
-        else:
-            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
 
 
 def test_max_depth():
