535f6d11932881b84a45e6f8515e60a6931f6ff1
Josh Lowe <joshlowe.cs@gmail.com>
2024-06-21 16:32:15 +0000

    Improved Solution for 703. Kth Largest Element in a Stream

--- algorithms/703. Kth Largest Element in a Stream/solution.py
+++ algorithms/703. Kth Largest Element in a Stream/solution.py
@@ -5,18 +5,15 @@ import heapq
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.heap = []
-        self.k = k
-        for num in nums:
-            heapq.heappush(self.heap, num)
-            if len(self.heap) > self.k:
-                heapq.heappop(self.heap)
+        self.heap, self.k = nums, k
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
 
     def add(self, val: int) -> int:
-        if len(self.heap) < self.k:
-            heapq.heappush(self.heap, val)
-        else:
-            heapq.heappushpop(self.heap, val)
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
         return self.heap[0]
 
 
