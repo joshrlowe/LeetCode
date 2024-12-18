e66a765c7e4b02c7d21a4c7b35d9015f0af6ec4b
Josh Lowe <joshlowe.cs@gmail.com>
2024-12-18 10:41:23 -0500

    Refine Test Cases for 94. Binary Tree Inorder Traversal

---
+++ algorithms/94. Binary Tree Inorder Traversal/inorder_traversal.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+from utils import TreeNode
+
+
+
+
+
+
+def inorderTraversal(root: Optional[TreeNode]) -> List[int]:
+    def dfs(node):
+        if node:
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+
+    result = []
+    dfs(root)
+    return result
+
+
