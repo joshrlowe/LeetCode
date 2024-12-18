b93836ac81e49c7bbafed307d72d1bae19e9361b
Josh Lowe <joshlowe.cs@gmail.com>
2024-12-18 10:40:38 -0500

    Refine Test Cases for 90. Subsets II

---
+++ algorithms/90. Subsets II/subsets_2.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+
+def subsetsWithDup(nums: List[int]) -> List[List[int]]:
+    def backtrack(i):
+        if i >= len(nums):
+            subsets.append(current[:])
+            return
+
+        current.append(nums[i])
+        backtrack(i + 1)
+        current.pop()
+
+        while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            i += 1
+        backtrack(i + 1)
+
+    subsets, current = [], []
+    nums.sort()
+    backtrack(0)
+    return subsets
---
+++ algorithms/90. Subsets II/test_subsets_2.py
@@ -0,0 +1,23 @@
+import pytest  # type: ignore
+from subsets_2 import subsetsWithDup as subsets_with_dup
+from typing import List
+
+@pytest.mark.parametrize("nums, expected", [
+    ([], [[]]),
+    ([1], [[], [1]]),
+    ([1, 2], [[], [1], [2], [1, 2]]),
+    ([1, 1], [[], [1], [1, 1]]),
+    ([1, 2, 2], [[], [1], [2], [2, 2], [1, 2], [1, 2, 2]]),
+    ([2, 1, 2], [[], [1], [2], [2, 2], [1, 2], [1, 2, 2]]),
+    ([1, 2, 3], [[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]),
+    ([1, 1, 1], [[], [1], [1, 1], [1, 1, 1]]),
+    ([4, 4, 4, 1, 4], [[], [1], [4], [4, 4], [4, 4, 4], [4, 4, 4, 4], [1, 4], 
+                      [1, 4, 4], [1, 4, 4, 4], [1, 4, 4, 4, 4]]),
+    ([1, 2, 2, 3], [[], [1], [2], [2, 2], [3], [1, 2], [1, 2, 2], [1, 3], 
+                    [2, 3], [2, 2, 3], [1, 2, 3], [1, 2, 2, 3]]),
+])
+def test_subsets_with_dup(nums: List[int], expected: List[List[int]]) -> None:
+    result = subsets_with_dup(nums)
+    result.sort()
+    expected.sort()
+    assert result == expected, f"nums: {nums}, expected: {expected}, result: {result}"
